2f158585c3c7f9042edaaa90c947684053d2b3db
donghunlee <makjang@knu.ac.kr>
2024-06-25 20:05:56 +0900

    프래임3

--- PhoneAssistant.py
+++ PhoneAssistant.py
@@ -1,2 +1,62 @@
 class PhoneAssistant:
     def __init__(self):
+
+
+
+
+
+    def Showme(self):
+        if self.price='''가장 기기값이 낮은 경우'''
+            f1=Frame('''''')
+                Picture=Label(f1,'''그림파일 넣는 코드''');Picture.grid(row=0,column=0,rowspan=4)'''폰 사진 넣을곳'''
+                TSS=Label(f1,text='통신사');TSS.grid(row=0,column=1)'''통T신S사S'''
+                ShowTSS=Label(f1,text='''통신사 넣는 코드''');ShowTSS.grid(row=0,column=2)
+                YGJ=Label(f1,text='요금제');YGJ.grid(row=1,column=1)'''요Y금G제G'''
+                ShowYGJ=Label(f1,text='''요금제 넣는 코드''');ShowYGJ.grid(row=1,column=2)
+                MonthPay=Label(f1,text='월 납부요금');MonthPay.gird(row=2,column=1)
+                ShowMonthPay=Label(f1,text='''월 납부요금 넣는 코드''');ShowMonthPay.grid(row=2,column=2)
+                DeviceCost=Label(f1,text='총 기기값');DeviceCost.grid(row=3,column=1)
+                ShowDeviceCost=Label(f1,text='''총 기기값 넣는 코드''');ShowDeviceCost.grid(row=3,column=2)
+                f.grid('''*첫번째 위치''')
+        else if self.price=='''가장 월 납부요금이 낮은 경우'''
+            f1=Frame('''''')
+                Picture=Label(f1,'''그림파일 넣는 코드''');Picture.grid(row=0,column=0,rowspan=4)'''폰 사진 넣을곳'''
+                TSS=Label(f1,text='통신사');TSS.grid(row=0,column=1)'''통T신S사S'''
+                ShowTSS=Label(f1,text='''통신사 넣는 코드''');ShowTSS.grid(row=0,column=2)
+                YGJ=Label(f1,text='요금제');YGJ.grid(row=1,column=1)'''요Y금G제G'''
+                ShowYGJ=Label(f1,text='''요금제 넣는 코드''');ShowYGJ.grid(row=1,column=2)
+                MonthPay=Label(f1,text='월 납부요금');MonthPay.gird(row=2,column=1)
+                ShowMonthPay=Label(f1,text='''월 납부요금 넣는 코드''');ShowMonthPay.grid(row=2,column=2)
+                DeviceCost=Label(f1,text='총 기기값');DeviceCost.grid(row=3,column=1)
+                ShowDeviceCost=Label(f1,text='''총 기기값 넣는 코드''');ShowDeviceCost.grid(row=3,column=2)
+                f.grid('''두번째 위치''')
+        else if self.price=='''추천1''':
+            f1=Frame('''''')
+                Picture=Label(f1,'''그림파일 넣는 코드''');Picture.grid(row=0,column=0,rowspan=4)'''폰 사진 넣을곳'''
+                TSS=Label(f1,text='통신사');TSS.grid(row=0,column=1)'''통T신S사S'''
+                ShowTSS=Label(f1,text='''통신사 넣는 코드''');ShowTSS.grid(row=0,column=2)
+                YGJ=Label(f1,text='요금제');YGJ.grid(row=1,column=1)'''요Y금G제G'''
+                ShowYGJ=Label(f1,text='''요금제 넣는 코드''');ShowYGJ.grid(row=1,column=2)
+                MonthPay=Label(f1,text='월 납부요금');MonthPay.gird(row=2,column=1)
+                ShowMonthPay=Label(f1,text='''월 납부요금 넣는 코드''');ShowMonthPay.grid(row=2,column=2)
+                DeviceCost=Label(f1,text='총 기기값');DeviceCost.grid(row=3,column=1)
+                ShowDeviceCost=Label(f1,text='''총 기기값 넣는 코드''');ShowDeviceCost.grid(row=3,column=2)
+                f.grid('''세번째''')
+        else if self.price=='''추천2''':
+            f1=Frame('''''')
+                Picture=Label(f1,'''그림파일 넣는 코드''');Picture.grid(row=0,column=0,rowspan=4)'''폰 사진 넣을곳'''
+                TSS=Label(f1,text='통신사');TSS.grid(row=0,column=1)'''통T신S사S'''
+                ShowTSS=Label(f1,text='''통신사 넣는 코드''');ShowTSS.grid(row=0,column=2)
+                YGJ=Label(f1,text='요금제');YGJ.grid(row=1,column=1)'''요Y금G제G'''
+                ShowYGJ=Label(f1,text='''요금제 넣는 코드''');ShowYGJ.grid(row=1,column=2)
+                MonthPay=Label(f1,text='월 납부요금');MonthPay.gird(row=2,column=1)
+                ShowMonthPay=Label(f1,text='''월 납부요금 넣는 코드''');ShowMonthPay.grid(row=2,column=2)
+                DeviceCost=Label(f1,text='총 기기값');DeviceCost.grid(row=3,column=1)
+                ShowDeviceCost=Label(f1,text='''총 기기값 넣는 코드''');ShowDeviceCost.grid(row=3,column=2)
+                f.grid('''네번째 위치''')
+
+MinDevice=Label('''해당 창''',text='가장 기기값이 낮음');grid('''좌표''')
+MinMonthPay=Label('''해당 창''',text='월 납부요금이 낮음');grid('''좌표''')
+Recommanded1=Label('''해당 창''',text='추천1');grid('''좌표''')
+Recommanded2=Label('''해당 창''',text='추천2');grid('''좌표''')
+'''오직 기초만을 위한 코드입니다. 여기서 분명 더 추가되거나 수정됄거에요. 아마 기능은 비슷할거에요. 자료가 어떻게 됄지 몰라서 일'''
